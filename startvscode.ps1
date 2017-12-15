d8b48fe2bec77fc307863e97c384b918889a7aaf
tksarah <tsarah8139@gmail.com>
2017-12-16 00:35:01 +0900

    update2

--- startvscode.ps1
+++ startvscode.ps1
@@ -1,4 +1,4 @@
 set-item env:DOCKER_HOST -value tcp://192.168.99.100:2376
-set-item env:DOCKER_CERT_PATH -value  'C:\Users\takeshik\.docker\machine\machines\default'
+set-item env:DOCKER_CERT_PATH -value  'C:\Users\hoge\.docker\machine\machines\default'
 set-item env:DOCKER_TLS_VERIFY -value 1
 Start-Process -FilePath 'C:\Program Files\Microsoft VS Code\Code.exe'
